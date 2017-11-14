c493046b1d863c7a06d791178b9536d017e6882f
Iti Tyagi <intechworx@gmail.com>
2017-11-14 12:31:10 +0530

    Add files via upload

---
+++ Arrays/Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        string result = string.Empty;
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            result += arr[i].ToString();
+            if (i != 0)
+            {
+                result += " ";
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
---
+++ Arrays/Hour Glass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int[][] arr = new int[6][];
+        int R = 6;
+        int C = 0;
+        for (int arr_i = 0; arr_i < 6; arr_i++)
+        {
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            arr[arr_i] = Array.ConvertAll(arr_temp, Int32.Parse);
+            C = arr.Length;
+        }
+
+        int Summax = 0;
+        int max = 0;
+        for (int i = 0; i < R-2; i++)
+        {
+            for (int j = 0; j  < C-2; j ++)
+            {
+                max = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2])
+                    + (arr[i + 1][j + 1])
+                    + (arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
+
+                if (max > Summax)
+                {
+                    Summax = max;
+                }
+            }
+        }
+
+        Console.WriteLine(Summax);
+        Console.ReadKey();
+    }
+}
+
